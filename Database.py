75b61c0b2712d25b2b1cf99af9dfde4fd72d381f
alskfl222 <alskfl222@kakao.com>
2021-12-13 22:40:38 +0900

    Testing for database connection modified:   Database.py     Function for connecting db modified:   test.ipynb

--- Database.py
+++ Database.py
@@ -4,18 +4,7 @@ from dotenv import load_dotenv
 
 class Database():
     def __init__(self):
-        load_dotenv()
-        DATABASE_HOST = os.getenv('DATABASE_HOST')
-        DATABASE_PORT = int(os.getenv('DATABASE_PORT'))
-        DATABASE_USER = os.getenv('DATABASE_USER')
-        DATABASE_PASSWORD = os.getenv('DATABASE_PASSWORD')
-        db = pymysql.connect(
-                host=DATABASE_HOST,
-                port=DATABASE_PORT,
-                user=DATABASE_USER,
-                passwd=DATABASE_PASSWORD,
-                charset='utf8mb4'
-        )
+        db = self.connect()
         cursor = db.cursor()
 
         with open("schema.sql", 'r') as f:
@@ -26,5 +15,33 @@ class Database():
         db.commit()
         db.close()
         print("DATABASE READY")
+    
+    def connect(self):
+        load_dotenv()
+        DATABASE_HOST = os.getenv('DATABASE_HOST')
+        DATABASE_PORT = int(os.getenv('DATABASE_PORT'))
+        DATABASE_DB = os.getenv('DATABASE_DB')
+        DATABASE_USER = os.getenv('DATABASE_USER')
+        DATABASE_PASSWORD = os.getenv('DATABASE_PASSWORD')
+        db = pymysql.connect(
+                host=DATABASE_HOST,
+                port=DATABASE_PORT,
+                db=DATABASE_DB,
+                user=DATABASE_USER,
+                passwd=DATABASE_PASSWORD,
+                charset='utf8mb4'
+        )
+        return db
 
+    def song_insert(self, list):
+        # print(list)
+        db = self.connect()
+        cursor = db.cursor()
+        sql = '''
+            DESCRIBE user;
+        '''
+        cursor.execute(sql)
+        res = cursor.fetchall()
+        db.commit()
+        db.close()
 
