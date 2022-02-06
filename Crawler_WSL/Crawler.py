f4b07361e822b2889dd444d918bdb2953669b71f
alskfl222 <alskfl222@kakao.com>
2022-02-06 21:24:35 +0900

    Practice GCP SQL connect

---
+++ Crawler_WSL/Crawler.py
@@ -0,0 +1,10 @@
+from Crawling import Crawling
+from Database import Database
+
+crawl = Crawling("날림v")
+BGM_data = crawl.get_BGM_data()
+
+DB = Database('alskfl')
+DB.update_song(BGM_data)
+DB.update_list(BGM_data)
+DB.update_song_list(BGM_data)
