b8818bf76b71a97cc0e0e3b0af63b01cbc1da291
alskfl222 <alskfl222@kakao.com>
2022-03-21 01:09:07 +0900

    Feat: 클라이언트 플레이어 페이지 유튜브 플레이어 & 시작, 정지 버튼 삽입

--- client/src/pages/Player.tsx
+++ client/src/pages/Player.tsx
@@ -1,23 +1,66 @@
 import { useEffect, useState } from 'react';
-import { useLocation } from 'react-router-dom'
-import { getPlayerItems } from '../apis'
-import styled from "styled-components";
+import { useLocation } from 'react-router-dom';
+import { getPlayerItems } from '../apis';
+import styled from 'styled-components';
 
 const Player = () => {
-  const { state } = useLocation()
-  const [ lists, setlists ] = useState<any>(state)
-  const hrefs = lists.map((el:any) => el.href)
-  console.log(hrefs)
+  const { state } = useLocation();
+  const [items, setItems] = useState<any[]>([]);
+  const [queue, setQueue] = useState<number>(0);
+  const hrefs = Array.isArray(state) ? state.map((el: any) => el.href) : [];
+  const player =
+    document.getElementsByTagName('iframe').length > 0
+      ? document.getElementsByTagName('iframe')[0].contentWindow
+      : null;
+
+  const makePlayer = (songHref: string) => {
+    return (
+      <iframe
+        width='480'
+        height='320'
+        src={`https://www.youtube.com/embed/${songHref}?autoplay=1&mute=0&enablejsapi=1&version=3&playerapiid=ytplayer`}
+        title='YouTube video player'
+        frameBorder='0'
+        allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
+        allowFullScreen
+      ></iframe>
+    );
+  };
+  const startPlayer = (player: HTMLIFrameElement['contentWindow']) => {
+    player?.postMessage(
+      '{ "event" : "command", "func" : "' + 'playVideo' + '" , "args" : "" }',
+      '*'
+    );
+  };
+
+  const stopPlayer = (player: HTMLIFrameElement['contentWindow']) => {
+    player?.postMessage(
+      '{ "event" : "command", "func" : "' + 'stopVideo' + '" , "args" : "" }',
+      '*'
+    );
+  };
 
   useEffect(() => {
-    getPlayerItems(hrefs).then(res => console.log(res))
-  })
+    const initPlayer = async () => {
+      const res = await getPlayerItems(hrefs);
+      await setItems(res.data);
+    };
+    initPlayer();
+    // eslint-disable-next-line
+  }, []);
+
+  console.log(items);
 
   return (
     <>
-    {typeof state}
+      {items.length > 0 && makePlayer(items[queue].songHref)}
+      <button onClick={() => startPlayer(player)}>start</button>
+      <button onClick={() => stopPlayer(player)}>stop</button>
+      {items.map((item) => {
+        return <div key={item.songHref}>{item.title}</div>;
+      })}
     </>
-  )
-}
+  );
+};
 
 export default Player;
